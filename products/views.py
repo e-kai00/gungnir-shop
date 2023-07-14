f76a42ab5c2ba4299f3d75be8ce72d7b6dc0b1a2
Katerina <yelkaterina@gmail.com>
2023-07-14 18:13:31 +0200

    Style product details page

--- products/views.py
+++ products/views.py
@@ -1,2 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Product
 
+# Create your views here.
+
+def product_detail(request, product_id):
+    """ A view to show individual product details """
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'products/product_detail.html', context)
