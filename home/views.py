f87086ba83ddab6b490a9585acb1e27d15c03b2c
Katerina <yelkaterina@gmail.com>
2023-07-14 15:20:59 +0200

    Display all products on home page

--- home/views.py
+++ home/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from products.models import Product
 
 
 def index(request):
 
-    return render(request, 'home/index.html')
+    products = Product.objects.all()
+
+    return render(request, 'home/index.html', {'products': products})
+
+ 
