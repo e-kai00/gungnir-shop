0fb7c5af4b33e29f586ab0ba2cc8f55f26baae9b
Katerina <yelkaterina@gmail.com>
2023-07-15 16:18:04 +0200

    Add feat: sort by category

--- home/views.py
+++ home/views.py
@@ -1,16 +1,26 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
-from products.models import Product
+from products.models import Product, Category
 
 
 def index(request):
 
     products = Product.objects.all()
     query = None
+    categories = None
 
     if request.GET:
-        if 'q' in request.GET:
+        # filter by category
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+            # display caregory name for user
+            categories = Category.objects.filter(name__in=categories)
+
+
+        # search site 
+        if 'q' in request.GET:            
             query = request.GET['q']
             if not query:
                 messages.error(request, "You didn't eneter any search criteria!")
@@ -22,6 +32,7 @@ def index(request):
     context = {
         'products': products,
         'search_term': query,
+        'current_categories': categories
     }
                 
 
