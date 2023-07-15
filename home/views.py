d6dd73ebbde5e9539e6a2ffa33a74209322f0d2e
Katerina <yelkaterina@gmail.com>
2023-07-15 15:38:26 +0200

    Add feat: search site

--- home/views.py
+++ home/views.py
@@ -1,11 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
 from products.models import Product
 
 
 def index(request):
 
     products = Product.objects.all()
+    query = None
 
-    return render(request, 'home/index.html', {'products': products})
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't eneter any search criteria!")
+                return redirect(reverse('home'))
+            
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            products = products.filter(queries)
+
+    context = {
+        'products': products,
+        'search_term': query,
+    }
+                
+
+    return render(request, 'home/index.html', context)
 
  
