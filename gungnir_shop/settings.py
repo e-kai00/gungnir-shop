06412b74c036792204ca5f3e8690f29357bf50bd
Katerina <yelkaterina@gmail.com>
2023-07-17 19:29:17 +0200

    Style toast messages

--- gungnir_shop/settings.py
+++ gungnir_shop/settings.py
@@ -164,5 +164,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-FREE_DELIVERY_THRESHOLD = 50   # remove later
+FREE_DELIVERY_THRESHOLD = 100   # remove later
 STANDARD_DELIVERY_PERCENTAGE = 10
