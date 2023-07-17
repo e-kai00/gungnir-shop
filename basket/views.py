9a09d7474d4c674a92e949f4b35dd528ea8df40d
Katerina <yelkaterina@gmail.com>
2023-07-17 12:06:35 +0200

    Add feat: update quantity and remove product from basket, calculate subtotal

--- basket/views.py
+++ basket/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, HttpResponse
 
 # Create your views here.
 def view_basket(request):
@@ -18,6 +18,32 @@ def add_to_basket(request, item_id):
     else:        
         basket[item_id] = quantity
 
-    request.session['basket'] = basket
-    
+    request.session['basket'] = basket    
     return redirect(redirect_url)
+
+
+def update_basket(request, item_id):
+
+    quantity = int(request.POST.get('quantity'))    
+    basket = request.session.get('basket', {})
+
+    # update quantity
+    if quantity > 0:
+        basket[item_id] = quantity
+    else:        
+        basket.pop(item_id)
+
+    request.session['basket'] = basket    
+    return redirect(reverse('view_basket'))
+
+
+def remove_from_basket(request, item_id):
+        
+    try:
+        basket = request.session.get('basket', {})
+        basket.pop(item_id)
+
+        request.session['basket'] = basket    
+        return HttpResponse(status=200)
+    except Exception as e:
+        return HttpResponse(status=500)
