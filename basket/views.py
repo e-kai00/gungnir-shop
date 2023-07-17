f7d12cd00aa2cc4e32099fc0120723402283d769
Katerina <yelkaterina@gmail.com>
2023-07-17 17:44:19 +0200

    Add feat: toasts

--- basket/views.py
+++ basket/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.contrib import messages
+from products.models import Product
 
 # Create your views here.
 def view_basket(request):
@@ -8,6 +10,7 @@ def view_basket(request):
 
 def add_to_basket(request, item_id):
 
+    product = Product.objects.get(pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     basket = request.session.get('basket', {})
@@ -17,6 +20,7 @@ def add_to_basket(request, item_id):
         basket[item_id] += quantity
     else:        
         basket[item_id] = quantity
+        messages.success(request, f'{product.name} added to basket')
 
     request.session['basket'] = basket    
     return redirect(redirect_url)
