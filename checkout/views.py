e90b85a3426f4970fecd9c2543ab4a54823c04da
Katerina <yelkaterina@gmail.com>
2023-07-18 20:35:44 +0200

    Style checkout page

--- checkout/views.py
+++ checkout/views.py
@@ -13,7 +13,9 @@ def checkout(request):
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
-        'order_form': order_form
+        'order_form': order_form,
+        'stripe_public_key': 'pk_test_51NQqzwHISqP7PCz88iYHXKXxhyzB7LZDVXo6KuvG3VjQA2wRfJM5ru5nBgrY6SW5PsJ4NFG0Vl2nYYywUu76Sj2600eGmTo526',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
