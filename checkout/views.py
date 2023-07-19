f26038496910cf0e18ac60ab3eb0fda277789dfb
Katerina <yelkaterina@gmail.com>
2023-07-19 21:45:05 +0200

    Add checkout_success view

--- checkout/views.py
+++ checkout/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.conf import settings
 from .forms import OrderForm
@@ -75,6 +75,25 @@ def checkout(request):
         'order_form': order_form,
         'stripe_public_key': stripe_public_key,
         'client_secret': intent.client_secret,
+    }
+
+    return render(request, template, context)
+
+
+def checkout_success(request, order_number):
+
+    safe_info = request.session.get('save_info')
+    order = get_object_or_404(Order, order_number=order_number)
+    messages.success(request, f"Your order has been processed successfully! \
+                     Your order number is {order_number}. Keep an eye on \
+                     your inbox, as we'll be sending a confirmation to {order.email}.")
+
+    if 'basket' in request.session:
+        del request.session['basket']
+
+    template = 'checkout/checkout_success.html'
+    context = {
+        'order': order
     }
 
     return render(request, template, context)
