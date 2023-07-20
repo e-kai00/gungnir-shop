5820c38a337ac0cd7666419644540fa45439f710
Katerina <yelkaterina@gmail.com>
2023-07-20 12:11:49 +0200

    Add checkout seccess template

--- checkout/models.py
+++ checkout/models.py
@@ -30,7 +30,7 @@ class Order(models.Model):
       
         # overide default behaviour - add a new field to the query set called lineitem_total__sum.
         # Which we can then get and set the order total to that.
-        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum']
+        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum'] or 0
         if self.order_total < settings.FREE_DELIVERY_THRESHOLD:
             self.delivery_cost = self.order_total * settings.STANDARD_DELIVERY_PERCENTAGE / 100
         else:
