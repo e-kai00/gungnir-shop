1b994b53c68c32392bbf1aff5cef816aeaa95988
Katerina <yelkaterina@gmail.com>
2023-07-23 15:01:46 +0200

    Add order history template structure and view

--- profiles/views.py
+++ profiles/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.contrib import messages
 from .models import UserProfile
 from .forms import UserProfileForm
+from checkout.models import Order
 
 
 def profile(request):
@@ -25,3 +26,21 @@ def profile(request):
     }
 
     return render(request, template, context)
+
+
+def order_history(request, order_number):
+
+    order = get_object_or_404(Order, order_number=order_number)
+
+    messages.info(request, f'This order has been made on {order.date}')
+
+    template = 'checkout/checkout.html'
+    context = {
+        'order': order,
+        'from_profile': True
+    }
+
+    return render(request, template, context)
+
+
+
