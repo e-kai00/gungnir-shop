7ec3e2be398351a3df2afeb81c4d93125e2682f3
Katerina <yelkaterina@gmail.com>
2023-07-24 17:46:27 +0200

    Add feat: add product

--- products/forms.py
+++ products/forms.py
@@ -10,9 +10,9 @@ class ProductForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        categories = Category.objects.all
+        categories = Category.objects.all()
         friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
 
         self.fields['category'].choices = friendly_names
         for field_name, field in self.fields.items():
-            field.widget.attr['class'] = 'rounded-pill'
+            field.widget.attrs['class'] = 'rounded-0'
--- products/views.py
+++ products/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Product
+from .forms import ProductForm
 
-# Create your views here.
 
 def product_detail(request, product_id):
     """ A view to show individual product details """
@@ -13,3 +13,14 @@ def product_detail(request, product_id):
     }
 
     return render(request, 'products/product_detail.html', context)
+
+
+def add_product(request):
+    """ Add a product to the store """
+    form = ProductForm()
+    template = 'products/add_product.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
