af762aeb62709ca1621ed1de270cc7cc4efed4c1
Katerina <yelkaterina@gmail.com>
2023-07-28 19:29:55 +0200

    Style coupon form

--- coupons/forms.py
+++ coupons/forms.py
@@ -4,3 +4,9 @@ from django import forms
 class ApplyCouponForm(forms.Form):
 
     code = forms.CharField()
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields:
+            self.fields[field].label = False
+            self.fields[field].widget.attrs['class'] = 'coupon_form'
