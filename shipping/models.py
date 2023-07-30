a877a7c3418b357d15239af8af327446554f871a
Katerina <yelkaterina@gmail.com>
2023-07-30 12:41:43 +0200

    Update model to include range of days

--- shipping/models.py
+++ shipping/models.py
@@ -1,25 +1,29 @@
 from django.db import models
-
-from django.contrib.postgres.fields import DateRangeField
+from django.contrib.postgres.fields import IntegerRangeField
 from datetime import datetime, timedelta
 
 
 class Shipping(models.Model):
 
     method = models.CharField(max_length=50)
-    price = models.DecimalField(max_digits=6, decimal_places=2) 
-    delivery_time = DateRangeField()
-    processing_time = models.PositiveBigIntegerField(default=1)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    delivery_time = IntegerRangeField(default=[10,21])                                 
+    processing_time = models.IntegerField(default=1)
 
 
     def __str__(self):
         return self.method
 
 
-    def estimated_delivery(self):
+    def estimated_delivery(self):  
         
         today = datetime.now().date()
-        estimated_delivery_time = today + timedelta(days=self.delivery_time + self.processing_time)
-        return estimated_delivery_time
+        min_time_delivery, max_time_delivery = self.delivery_time.lower, self.delivery_time.upper
+        
+        min_estimated_delivery = today + timedelta(days=min_time_delivery + self.processing_time)
+        max_estimated_delivery = today + timedelta(days=max_time_delivery + self.processing_time)
+        estimated_delivery_range = f'{min_estimated_delivery} - {max_estimated_delivery}'
+
+        return estimated_delivery_range
 
 
