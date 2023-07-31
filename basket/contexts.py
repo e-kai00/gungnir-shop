d59c080b99d63e7eade7717ed69bee5e3c079f0b
Katerina <yelkaterina@gmail.com>
2023-07-31 13:58:14 +0200

    Add logic to add shipping costs to basket total

--- basket/contexts.py
+++ basket/contexts.py
@@ -4,6 +4,7 @@ from decimal import Decimal
 from django.shortcuts import get_object_or_404
 from products.models import Product
 from coupons.models import Coupon
+from shipping.models import Shipping
 from coupons.forms import ApplyCouponForm
 from shipping.forms import ShippingForm
 
@@ -11,12 +12,12 @@ from shipping.forms import ShippingForm
 
 def basket_contents(request):
 
+    # basket items
     basket_items = []
     total = 0 
     product_count = 0
     basket = request.session.get('basket', {})
-    # coupon_id = request.session.get('coupon_id')
-
+    
     for item_id, quantity in basket.items():
         product = get_object_or_404(Product, pk=item_id)
         total += quantity * product.price
@@ -27,40 +28,53 @@ def basket_contents(request):
             'product': product
         })
 
-    if total < settings.FREE_DELIVERY_THRESHOLD:
-        delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)  # remove later
-        free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total        
-    else:
-        delivery = 0
-        free_delivery_delta = 0
+    # if total < settings.FREE_DELIVERY_THRESHOLD:
+    #     delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100) 
+    #     free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total        
+    # else:
+    #     delivery = 0
+    #     free_delivery_delta = 0
 
+    # shipping
+    shipping_form = ShippingForm()
+    shipping_id = request.session.get('shipping_id')    
+    try:
+        if shipping_id is not None:
+            shipping = Shipping.objects.get(id=shipping_id)
+            shipping_cost = shipping.price
+        else:
+            shipping = None
+            shipping_cost = 0
+    except ObjectDoesNotExist:
+        shipping = None
+        shipping_cost = 0
+
+    # coupon
+    apply_coupon_form = ApplyCouponForm()
     coupon_id = request.session.get('coupon_id')
     try:        
         if coupon_id:
             coupon = Coupon.objects.get(id=coupon_id)
             discount = total * (coupon.value / Decimal(100))
             total_with_coupon = total - discount        
-            grand_total = delivery + total_with_coupon
+            grand_total = shipping_cost + total_with_coupon
         else:
             coupon = None
             discount = 0
-            grand_total = delivery + total + discount
+            grand_total = shipping_cost + total + discount
     except ObjectDoesNotExist:
             coupon = None
             discount = 0
-            grand_total = delivery + total + discount
-
+            grand_total = shipping_cost + total + discount    
     
-    apply_coupon_form = ApplyCouponForm()
-    shipping_form = ShippingForm()
-
+    
     context = {
         'basket_items': basket_items,
         'total': total,
         'product_count': product_count,
-        'delivery': delivery,
-        'free_delivery_delta': free_delivery_delta,
-        'free_delivery_threshold': settings.FREE_DELIVERY_THRESHOLD,
+        'delivery': shipping_cost,
+        # 'free_delivery_delta': free_delivery_delta,
+        # 'free_delivery_threshold': settings.FREE_DELIVERY_THRESHOLD,
         'coupon': coupon,
         'discount': discount,        
         'apply_coupon_form': apply_coupon_form,
--- shipping/views.py
+++ shipping/views.py
@@ -7,18 +7,12 @@ def shipping_options(request):
     if request.method == 'POST':
         shipping_form = ShippingForm(request.POST)
         if shipping_form.is_valid():
-            shipping_option = shipping_form.cleaned_data['shipping']
-            shipping_price = shipping_option.price
-            shipping_method = shipping_option.method
-        return redirect(reverse('view_basket'))
+            shipping = shipping_form.cleaned_data['shipping']
+            # shipping_price = shipping_option.price
+            # shipping_method = shipping_option.method
+            request.session['shipping_id'] = shipping.id
     else:
         shipping_form = ShippingForm()
-
-    template = 'shipping/shipping.html'
-    
-    context = {
-        'shipping_form': shipping_form,               
-    }
-
-    return render(request, template, context)
+        
+    return redirect(reverse('view_basket'))
     
