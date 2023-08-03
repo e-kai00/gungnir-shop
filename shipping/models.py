2d1181684c837f5c3c5edd79ba1aef8b191ee815
Katerina <yelkaterina@gmail.com>
2023-08-03 19:50:46 +0200

    Tidy-ups shipping app

--- shipping/models.py
+++ shipping/models.py
@@ -15,7 +15,14 @@ class Shipping(models.Model):
         return f"${self.price} {self.method}"
 
 
-    def estimated_delivery(self):  
+    def estimated_delivery(self):
+        """
+        Calculate estimated delivery range based on the 'delivery_time_min',
+        'delivery_time_max', and 'processing_time' of the model instance.
+        The delivery range is represented as string in the format "day month - day month",
+        where the first day is the earliest estimated delivery and the second day is 
+        the latest estimated delivery date.
+        """
         
         today = datetime.now().date()       
         
@@ -27,6 +34,10 @@ class Shipping(models.Model):
     
 
     def order_dispatch(self):
+        """
+        Calculate dispatch date based on 'processing_time' of the model instance.
+        The dispatch date is represented as string in the format "day month".
+        """
 
         today = datetime.now().date()
         dispatch_date = today + timedelta(days=self.processing_time)
--- shipping/views.py
+++ shipping/views.py
@@ -1,15 +1,21 @@
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import redirect, reverse
 from .forms import ShippingForm
 
 
-def shipping_options(request):   
+def shipping_options(request):
+    """
+    Set selected shipping option for the current user's session.
+    If request method is POST, it validates submitted 
+    shipping option from form. If form is valid, it sets
+    'shipping_id' key in the request session to the ID of selected shipping.
+    If request method is GET, it displays ShippingForm to allow 
+    to select shipping option.
+    """
 
     if request.method == 'POST':
         shipping_form = ShippingForm(request.POST)
         if shipping_form.is_valid():
-            shipping = shipping_form.cleaned_data['shipping']
-            # shipping_price = shipping_option.price
-            # shipping_method = shipping_option.method
+            shipping = shipping_form.cleaned_data['shipping']            
             request.session['shipping_id'] = shipping.id
     else:
         shipping_form = ShippingForm()
