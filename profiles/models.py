d00935548dd0b3e4e073de1a3d773570e6635a18
Katerina <yelkaterina@gmail.com>
2023-08-04 19:09:55 +0200

    Add ordering from newest to oldest oders on user's order history

--- profiles/models.py
+++ profiles/models.py
@@ -17,6 +17,7 @@ class UserProfile(models.Model):
     default_country = CountryField(blank_label='Country', null=True, blank=True)
 
     def __str__(self):
+        
         return self.user.username
     
     @receiver(post_save, sender=User)    
